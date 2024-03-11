fe5310c93f4cfd9e8c429434a929f7915cce869d
Scoob1 <tamara_walling@yahoo.com>
2024-03-11 23:49:32 +0000

    readme,main,task0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. if it fails, return 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	f = open(filename, O_RDONLY);
+
+	if (f == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(f, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(f);
+
+	free(buf);
+
+	return (nwr);
+}
