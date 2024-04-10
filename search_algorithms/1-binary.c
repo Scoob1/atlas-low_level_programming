3cfc610eb0a1dc585b1bc06ac5ba5863b4adb562
Scoob1 <tamara_walling@yahoo.com>
2024-04-10 20:07:37 +0000

    edited spacing andadded to header

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -13,19 +13,19 @@
 
 int binary_search(int *array, size_t size, int value)
 {
+	size_t left, right, mid, i;
+
 	if (array == NULL)
 		return (-1);
-	
-	size_t left = 0;
-	size_t right = size - 1;
+	left = 0;
+	right = size - 1;
 
 	while (left <= right)
 	{
-		size_t mid = (left + right) / 2;
+		mid = (left + right) / 2;
 
 		printf("Searching in array; ");
-		
-		for (size_t i = left; i <= right; i++)
+		for (i = left; i <= right; i++)
 		{
 			printf("%d", array[1]);
 			if (i < right)
@@ -34,11 +34,10 @@ int binary_search(int *array, size_t size, int value)
 		printf("\n");
 		if (array[mid] == value)
 			return (mid);
-		
 		else if (array[mid] > value)
 			right = mid - 1;
-		
-		else left = mid + 1;
+		else
+			left = mid + 1;
 	}
 
 	return (-1);
