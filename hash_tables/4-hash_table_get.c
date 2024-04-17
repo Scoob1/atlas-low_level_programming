337f071acb7de987f03fbe65a89aee289971495a
Scoob1 <tamara_walling@yahoo.com>
2024-04-17 19:32:16 +0000

    semicolon in header

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,32 +1,29 @@
 #include "hash_tables.h"
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * hash_table_get - Retrieves a value associated with a key.
- * @ht: The hash table to look into.
- * @key: The key to search for.
+ * hash_table_get- retrieves a value associated with a key
+ * @ht: hash table you want to look into
+ * @key: the key you are looking for
  *
- * Return: The value associated with the element
- * or NULL if key couldn't be found.
+ * Return: value associated with the element, or NULL if not found
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
-	hash_node_t *node;
+	hash_node_t *temp;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	node = ht->array[index];
 
-	while (node != NULL)
+	temp = ht->array[index];
+	while (temp != NULL)
 	{
-		if (strcmp(node->key, key) == 0)
-			return (node->value);
-		node = node->next;
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
 	}
 
 	return (NULL);
