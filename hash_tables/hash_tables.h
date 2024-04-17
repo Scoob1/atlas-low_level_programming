2976bb205c5262d5bccb9e71e0618817c45f0b41
Scoob1 <tamara_walling@yahoo.com>
2024-04-17 19:19:51 +0000

    get

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -2,7 +2,7 @@
 #define HASH_TABLES_H
 
 #include <stdlib.h>
-
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -41,4 +41,7 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+
 #endif
