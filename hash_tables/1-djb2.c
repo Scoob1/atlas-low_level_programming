d338dcc39ee615edd4d9d72e6f79f48c28476027
Scoob1 <tamara_walling@yahoo.com>
2024-04-17 19:01:58 +0000

    djb2

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,1 +1,21 @@
-fgwgs
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implents the hash of djb2
+ * @str: inputs the string to hash
+ *
+ * Return: the computed hash value
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+
+	int c;
+
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
+}
